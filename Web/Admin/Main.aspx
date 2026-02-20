4d1c8994387694814ec64e90713a63eb5d3d8e69
yusuphardz-cell <yusup.hardz@gmail.com>
2026-02-20 14:26:54 +0700

    Add files via upload

---
+++ Web/Admin/Main.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Admin_Main" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" runat="server" contentplaceholderid="ContentPlaceHolder1">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SystemLeague">
+        <Columns>
+            <asp:BoundField DataField="Id" HeaderText="Id" SortExpression="Id" />
+            <asp:BoundField DataField="Nickname" HeaderText="Nickname" SortExpression="Nickname" />
+            <asp:BoundField DataField="Contrasena" HeaderText="Contrasena" SortExpression="Contrasena" />
+            <asp:BoundField DataField="Mail" HeaderText="Mail" SortExpression="Mail" />
+            <asp:BoundField DataField="F_reg" HeaderText="F_reg" SortExpression="F_reg" />
+        </Columns>
+    </asp:GridView>
+    <asp:SqlDataSource ID="SystemLeague" runat="server" ConnectionString="<%$ ConnectionStrings:SystemLeagueConnectionString %>" SelectCommand="SELECT * FROM [Player]"></asp:SqlDataSource>
+</asp:Content>
+
+
---
+++ Web/User/Registro.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Biblioteca;
+using System.Threading;
+using Biblioteca.Negocio;
+
+public partial class User_Registro : System.Web.UI.Page
+{
+
+    PlayerColeccion playerColeccion = new PlayerColeccion();
+
+
+    private Player Jugador
+    {
+        get
+        {
+            return (Player)Session["JugadorSession"];
+        }
+
+    }
+    private PlayerColeccion ListaJugador
+    {
+        get
+        {
+            if (Session["ListaJugadorSession"] == null)
+            {
+                Session["ListaJugadorSession"] = new PlayerColeccion();
+            }
+            return (PlayerColeccion)Session["ListaJugadorSession"];
+        }
+        set
+        {
+            Session["ListaJugadorSession"] = value;
+        }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Jugador != null)
+        {
+            Response.Redirect("/Inicio.aspx");
+        }
+    }
+    private int generarId(DateTime time)
+    {
+        //int c = ListaJugador.count++;
+        return int.Parse(time.ToString("HHmmss"));
+    }
+
+    protected void btnRegistrar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (playerColeccion != null)
+            {
+                if (playerColeccion.ComprobarNick(txtUsuario.Text))
+                {
+                    throw new ArgumentException("El Nombre de usuario ya existe\nIntente con otro diferente");
+                }
+            }
+
+            if (!txtPass.Text.Equals(txtPass2.Text))
+            {
+                throw new ArgumentException("Las contraseñas no coinciden");
+            }
+
+            if (!txtMail.Text.Equals(txtMail2.Text))
+            {
+                throw new ArgumentException("Las mail no coinciden");
+            }
+
+            Player player = new Player();            
+
+            player.FechaRegistro = DateTime.Now;
+            player.Id = generarId(player.FechaRegistro);
+            player.Nick = txtUsuario.Text;
+            player.Pass = txtPass.Text;
+            player.Email = txtMail.Text;
+
+            player.Create();
+
+            lblOuput.Text = string.Format("Jugador {0}, registrado con éxito", player.Nick);
+            Response.Redirect("/Inicio.aspx");
+        }
+        catch (Exception ex)
+        {
+            lblOuput.Text = ex.Message;
+        }
+    }
+}
